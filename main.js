1b891df4af2d8077b3748b52fad33b3595a67de5
Alma Valeria <almamartinez19682@gmail.com>
2024-08-29 21:27:37 -0600

    Add files via upload
    Aquí esta mi portafolio Alma Valeria

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #fb4993;'>|</span>",
+ cursor: "<span style='color: black;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #fb4993;">Enseño programación web y hago contenido de tecnología.</span>')
+  .typeString('<span style="color:#e70646;">Soy estudiante de Front-end en el curso de Tecnolochicapro</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
